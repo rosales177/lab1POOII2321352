1197abe1315a6c5b36565c7f40f37150efdae0e2
yaasss26 <iamileariana@gmail.com>
2023-09-13 22:51:01 -0500

    Implementacion de observer

--- Observer/com.observer/src/com/observer/ComObserver.java
+++ Observer/com.observer/src/com/observer/ComObserver.java
@@ -21,6 +21,31 @@ public class ComObserver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Teleticket teleticket = new Teleticket();
+
+        Fanatico fan1 = new Fanatico("Manuel", "Zegarra");
+        Fanatico fan2 = new Fanatico("Geraldo", "Rosales");
+
+        teleticket.addObserver(fan1);
+        teleticket.addObserver(fan2);
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+          @Override
+          public void run() {
+            Calendar calendar = Calendar.getInstance();
+            int diaDelMes = calendar.get(Calendar.DAY_OF_MONTH);
+            if (diaDelMes == 15) {
+              teleticket.setMensaje("Entradas disponibles para el concierto de su artista.");
+            }
+          }
+        }, new Date(), 24 * 60 * 60 * 1000);
+
+        teleticket.setMensaje("Nuevo mensaje de Teleticket");
+
+        teleticket.removeObserver(fan2);
+
+        teleticket.setMensaje("Otro mensaje de Teleticket");
     }
     
 }
