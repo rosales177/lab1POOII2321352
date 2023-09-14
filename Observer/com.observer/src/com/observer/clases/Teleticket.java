e99ba0ff2f448e827a50e9cb998d000b14551824
rosales177 <rosalespoma15@gmail.com>
2023-09-13 22:20:48 -0500

    Creacion de clase Teleticket

--- Observer/com.observer/src/com/observer/clases/Teleticket.java
+++ Observer/com.observer/src/com/observer/clases/Teleticket.java
@@ -4,6 +4,8 @@
  */
 package com.observer.clases;
 
+import java.util.ArrayList;
+import java.util.List;
 
 interface Observer{
     void update(String mensaje);
@@ -13,6 +15,28 @@ interface Observer{
  *
  * @author rosales1015
  */
+
+
 public class Teleticket {
-    
+    private List<Observer> observadores = new ArrayList<>();
+    private String mensaje;
+
+    public void addObserver(Observer observer) {
+        observadores.add(observer);
+    }
+
+    public void removeObserver(Observer observer) {
+        observadores.remove(observer);
+    }
+
+    public void notificar() {
+        for (Observer observer : observadores) {
+            observer.update(mensaje);
+        }
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+        notificar();
+    }
 }
