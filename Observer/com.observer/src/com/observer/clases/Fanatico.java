6bd412415314fbdb02fbedce238690fb08b3f81d
rosales177 <rosalespoma15@gmail.com>
2023-09-13 21:53:37 -0500

    Creacion de archivos

---
+++ Observer/com.observer/src/com/observer/clases/Fanatico.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.observer.clases;
+
+/**
+ *
+ * @author rosales1015
+ */
+public class Fanatico {
+    
+}
---
+++ Observer/com.observer/src/com/observer/clases/Teleticket.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.observer.clases;
+
+
+interface Observer{
+    void update(String mensaje);
+}
+
+/**
+ *
+ * @author rosales1015
+ */
+public class Teleticket {
+    
+}
